ef4713b883cc752b9397190dc38b62fb4e8f69e2
yarinbaslo <96728398+yarinbaslo@users.noreply.github.com>
2025-04-16 00:31:41 +0300

    Update yarinbaslo.java

--- yarinbaslo.java
+++ yarinbaslo.java
@@ -1,7 +1,7 @@
 public class yarinbaslo {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
-            System.out.println("yarin baslo");
+            System.out.println("update yarin baslo...");
         }
     }
 }
